2129453691cc0ed2c2dc0c31afee3f347cf95c88
Дима <seyqo100qq@gmail.com>
2025-10-09 00:54:51 +0300

    Update BLACKLIST.lua

--- BLACKLIST.lua
+++ BLACKLIST.lua
@@ -1,19 +1,6 @@
 {
-    "players": [
-        {
-            "name": "715_GodAlt",
-            "id": "7268014557",
-            "reason": "TEST BAN!",
-            "banned_by": "Skaneexx",
-            "date": "2025-10-09"
-        },
-    ],
-    "hwids": [
-        {
-            "hwid": "hwid_hash_here",
-            "reason": "TEST BAN!",
-            "banned_by": "Skaneexx",
-            "date": "2025-10-09"
-        }
-    ]
+  "blacklist": [
+    {"type":"username","id":"715_GodAlt","reason":"test ban"},
+    {"type":"hwid","id":"SOME-HWID-STRING-123","reason":"test ban"},
+  ]
 }
